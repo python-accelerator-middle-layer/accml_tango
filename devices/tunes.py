c6ad62ae0ea7f68245bbb3b0bf8a986896c531fb
Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
2026-01-16 16:18:27 +0100

    [TASK] tunes: ported towards standard readable

--- devices/tunes.py
+++ devices/tunes.py
@@ -1,10 +1,9 @@
 from typing import Annotated as A
 
 from ophyd_async.core import SignalRW, StandardReadableFormat as Format
-from ophyd_async.tango.core import TangoReadable, TangoPolling
 
 
-class Tunes(TangoReadable):
+class Tunes(TangoDevice, StandardReadable):
     x: A[ SignalRW[float] , Format.HINTED_UNCACHED_SIGNAL , TangoPolling(.5, 0.01, 1e-4) ]
     y: A[ SignalRW[float] , Format.HINTED_UNCACHED_SIGNAL , TangoPolling(.5, 0.01, 1e-4) ]
     count : A[ SignalRW[int] , Format.HINTED_UNCACHED_SIGNAL  ]
