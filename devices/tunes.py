253e34c579f00a6cd606a20a4f09ce5550979a86
Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
2026-01-29 16:58:48 +0100

    [TASK] tunes: change precision in description document
    Idea: demonstrate what can be done to change representation if required

--- devices/tunes.py
+++ devices/tunes.py
@@ -1,5 +1,6 @@
 from typing import Annotated as A
 
+from event_model import DataKey
 from ophyd_async.core import SignalRW, StandardReadable, StandardReadableFormat as Format
 from ophyd_async.tango.core import TangoDevice, TangoPolling
 from ophyd_async.core import AsyncStatus
@@ -10,12 +11,21 @@ from accml.core.utils.ophyd_async.new_value import wait_for_new_value
 
 class Tunes(TangoDevice, StandardReadable, Triggerable):
     # fmt:off
-    hor:  A[ SignalRW[float] , Format.HINTED_UNCACHED_SIGNAL , TangoPolling(.5, 0.01, 1e-4) ]
-    vert: A[ SignalRW[float] , Format.HINTED_UNCACHED_SIGNAL , TangoPolling(.5, 0.01, 1e-4) ]
+    hor:  A[ SignalRW[float] , Format.HINTED_UNCACHED_SIGNAL , TangoPolling(.5, 1e-3, 1e-4) ]
+    vert: A[ SignalRW[float] , Format.HINTED_UNCACHED_SIGNAL , TangoPolling(.5, 1e-3, 1e-4) ]
     # fmt:on
 
     @AsyncStatus.wrap
     async def trigger(self) -> Status:
         await wait_for_new_value(self.vert)
+
+
+    async def describe(self) -> dict[str, DataKey]:
+        """adjust precision to a higher value
+        """
+        d = await super().describe()
+        for name in ["hor", "vert"]:
+            d[f"{self.name}-{name}"]["precision"] = 5
+        return d
 
     # count : A[ SignalRW[int] , Format.HINTED_UNCACHED_SIGNAL  ]
