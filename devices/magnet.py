a5219337ad3f331d1b5c3af9790953cd57f70efb
Waheed <wsulimankhail@gmail.com>
2026-01-30 13:02:25 +0100

    [TASK] soleil magnet device attributes based on their twin

--- devices/magnet.py
+++ devices/magnet.py
@@ -1,13 +1,12 @@
 from typing import Annotated as A
 
-from ophyd_async.tango.core import TangoDevice, TangoPolling
 from ophyd_async.core import (
     AsyncStageable,
-    SignalR,
     SignalRW,
     StandardReadable,
     StandardReadableFormat as Format
 )
+from ophyd_async.tango.core import TangoDevice, TangoPolling
 
 
 class Magnet(TangoDevice, StandardReadable, AsyncStageable):
@@ -17,9 +16,9 @@ class Magnet(TangoDevice, StandardReadable, AsyncStageable):
     """
 
     # fmt: off
-    magnetic_strength:          A[ SignalRW[float] , Format.HINTED_UNCACHED_SIGNAL , TangoPolling(0.2, 1e-4, 1e-4) ]
-    magnetic_strength_readback: A[ SignalR[float]  , Format.UNCACHED_SIGNAL        , TangoPolling(0.1, 1e-4, 1e-4) ]
-    current :                   A[ SignalRW[float] , Format.UNCACHED_SIGNAL        , TangoPolling(0.1, 0.01, 1e-4) ]
-    x_kick :                    A[ SignalRW[float] , Format.UNCACHED_SIGNAL        , TangoPolling(0.1, 0.01, 1e-4) ]
-    y_kick :                    A[ SignalRW[float] , Format.UNCACHED_SIGNAL        , TangoPolling(0.1, 0.01, 1e-4) ]
+    Strength: A[SignalRW[float], Format.HINTED_UNCACHED_SIGNAL, TangoPolling(0.2, 1e-4, 1e-4)]
+    # magnetic_strength_readback: A[ SignalR[float]  , Format.UNCACHED_SIGNAL        , TangoPolling(0.1, 1e-4, 1e-4) ]
+    # current :                   A[ SignalRW[float] , Format.UNCACHED_SIGNAL        , TangoPolling(0.1, 0.01, 1e-4) ]
+    # x_kick :                    A[ SignalRW[float] , Format.UNCACHED_SIGNAL        , TangoPolling(0.1, 0.01, 1e-4) ]
+    # y_kick :                    A[ SignalRW[float] , Format.UNCACHED_SIGNAL        , TangoPolling(0.1, 0.01, 1e-4) ]
     # fmt: on
